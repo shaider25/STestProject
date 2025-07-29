a63e0315e9b2cfd62b06adec8fba525fb1e479e1
shaider25 <syeda.trackgrp@gmail.com>
2025-07-29 14:35:18 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+
+using System;
+using System.Data.SqlClient;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -23,3 +26,30 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
+
+
+public class UserLogin
+{
+    public void Login(string username, string password)
+    {
+        string connectionString = "your_connection_string_here";
+        string query = $"SELECT * FROM Users WHERE Username = '{username}' AND Password = '{password}'";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            SqlCommand command = new SqlCommand(query, connection);
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                Console.WriteLine("Login successful!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid credentials.");
+            }
+        }
+    }
+}
